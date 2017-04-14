e1ef327f7b0e133e1e5b9d96167491c4607ba70f
Matthias Winkler <mwinkler@gmx.ch>
2017-04-14 02:24:38 +0200

    Close

--- src/main.js
+++ src/main.js
@@ -6,12 +6,15 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow() {
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
     // Create the browser window.
-    win = new BrowserWindow({
+    win = new BrowserWindow(/*{
         width: 800,
         height: 600
-    });
+    }*/);
 
     win.setMenu(null);
 
@@ -32,34 +35,18 @@ function createWindow() {
         // when you should delete the corresponding element.
         win = null
     });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-});
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
-    }
+    app.quit()
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
 ipcMain.on('bye', (event, arg) => {
-  console.log(arg)  // prints "ping"
+  //console.log(arg)  // prints "ping"
   //event.sender.send('asynchronous-reply', 'pong')
+  app.quit();
 })
