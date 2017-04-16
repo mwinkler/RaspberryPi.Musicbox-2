a4600e371ca1c3c3cd0d66091a5e80cf84c3f2ba
Matthias Winkler <mwinkler@gmx.ch>
2017-04-16 03:10:43 +0200

    Hide dev tools

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ app.on('ready', () => {
     }));
 
     // Open the DevTools.
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
