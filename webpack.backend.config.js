9d93bd4a7f66b80c83104adb91e0c167e60dc7da
Matthias Winkler <mwinkler@gmx.ch>
2017-04-17 21:24:22 +0200

    Add packaging

--- webpack.backend.config.js
+++ webpack.backend.config.js
@@ -1,17 +1,18 @@
 
 const path = require('path');
 const webpack = require('webpack');
+const Copy = require('copy-webpack-plugin');
 
 // vars
 const isProd = (process.env.NODE_ENV === 'production');
-const src = 'src';
+const src = 'src/backend';
 const output = 'build';
 const filename = `[name]${isProd ? '-[hash:6]' : ''}`;
 const filenameExt = filename + '.[ext]';
 
 module.exports = {
     entry: {
-        main: path.resolve(src, 'backend', 'main.ts')
+        main: path.resolve(src, 'main.ts')
     },
     output: {
         path: path.resolve(__dirname, output),
@@ -30,6 +31,9 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     target: 'electron-main',
+    plugins: [
+        new Copy([{ from: path.resolve(src, 'package.json') }])
+    ],
     node: {
         __dirname: false,
         __filename: false
