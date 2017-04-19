b21ceea52e7f2732873517045601630e331ecd19
Matthias Winkler <Matthias.Winkler@swisscom.com>
2017-04-19 08:46:54 +0200

    Add cover

---
+++ modules.d.ts
@@ -0,0 +1,8 @@
+
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.gif'
+declare module '*.html' {
+    var _: string;
+    export default _; 
+}
--- src/frontend/views/player.tsx
+++ src/frontend/views/player.tsx
@@ -2,19 +2,35 @@
 import * as React from 'react';
 import './player.scss';
 import IpcButton from '../components/ipc-button';
+import demoCover from '../images/cover.png';
+
+console.log(demoCover);
 
 interface IPlayerProperties {
-
+    cover?: string;
 }
 
 export default class Player extends React.Component<IPlayerProperties, {}> {
+
+    playerCoverImageStyles: React.CSSProperties;
+
+    constructor(props: IPlayerProperties) {
+        super(props);
+
+        this.playerCoverImageStyles = {
+            backgroundImage: `url(${demoCover})`,
+            backgroundColor: '#777'
+        }
+    }
 
     render() {
         return (
             <div className="player">
                 <div className="player-cover">
-                    Current Cover
-                    <div className="player-cover-image"></div>
+                    <div className="player-cover-image" style={this.playerCoverImageStyles}></div>
+                </div>
+                <div className="player-title">
+                    Current Title
                 </div>
                 <div className="player-controls">
                     <IpcButton command="play" icon="fa-play"></IpcButton>
