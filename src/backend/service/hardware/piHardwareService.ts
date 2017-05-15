bf0eb76d4eeb573d4c248f97aa528876b70920b9
Matthias Winkler <mwinkler@gmx.ch>
2017-05-16 00:02:04 +0200

    Gpio example

--- src/backend/service/hardware/piHardwareService.ts
+++ src/backend/service/hardware/piHardwareService.ts
@@ -4,7 +4,13 @@ import * as gpio from 'pi-gpio';
 const service: IHardwareService = {
 
     init: () => {
-        
+
+        // set boot pin to high
+        // gpio.open(16, "output", function(err) {		// Open pin 16 for output
+        //     gpio.write(16, 1, function() {			// Set pin 16 high (1)
+        //         gpio.close(16);						// Close pin 16
+        //     });
+        // });
     },
 
     shutdown: () => {
